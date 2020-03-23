3171516fe60efa9b65a0eb6fb0fa1a8c25a3194a
Alison Johnston <ali@macbook-pro-3.home>
2020-03-23 12:04:56 -0400

    tidying code, fixing bugs, adding plots

--- 06_a_occupancy_models_plot.R
+++ 06_a_occupancy_models_plot.R
@@ -16,6 +16,9 @@ library(readr)
 library(stringr)
 library(tidyr)
 library(lubridate)
+library(ggplot2)
+library(ggpubr)
+library(forcats)
 
 # resolve namespace conflicts
 select <- dplyr::select
@@ -78,9 +81,9 @@ ebird_habitat <- inner_join(ebird, habitat, by = "checklist_id") %>%
         mutate(time_observations_started = as.numeric(as.character(time_observations_started))) %>%
         mutate(number_observers = as.numeric(as.character(number_observers))) %>%
         mutate(duration_minutes = as.numeric(as.character(duration_minutes))) %>%
-        mutate(day_of_year = yday(observation_date))
-        mutate(locality_id = paste0(round(ebird_habitat$longitude, 6), "_", 
-          round(ebird_habitat$latitude, 6)))
+        mutate(day_of_year = yday(observation_date)) %>%
+        mutate(locality_id = paste0(round(longitude, 6), "_", 
+          round(latitude, 6)))
 
 
 # map data ----
@@ -106,7 +109,7 @@ bcr <- read_sf(f_gpkg, "bcr") %>%
 
 # define model set ----
 mod_set <- tibble(run_name = c("incomplete", "all", "complete", 
-                               "sss", "effort", "best_practice"),
+                               "sss", "effort", "covs"),
                   incomplete =        c(1, 0, 0, 0, 0, 0),
                   complete =          c(0, 0, 1, 1, 1, 1),
                   spatial_subsample = c(0, 0, 0, 1, 1, 1),
@@ -148,7 +151,7 @@ predict_raster <- function(model, data, template) {
   r_pred[[-1]]
 }
 
-r <- raster("data/modis_5xagg.tif")
+r <- raster(str_glue({data_folder}, "/modis_5xagg.tif"))
 mod_set <- mod_set %>% 
   mutate(r_pred = map(models, predict_raster, data = pred_surface, 
                       template = r))
@@ -224,105 +227,118 @@ dev.off()
 
 
 
-# --------------------------------------------------------------------
-# predict detectability
+# ####################################################################
+# PREDICT DETECTABILITY
 
-  # complete checklists only
-  pred_data <- filter(ebird_habitat, as.logical(all_species_reported))
-  # filter on effort covariates
-  pred_data <- pred_data %>%
-      mutate(duration_minutes = as.numeric(as.character(duration_minutes))) %>%      
-      filter(protocol_type %in% c("Stationary", "Traveling"),
-             effort_distance_km <= 5,
-             duration_minutes <= 5 * 60,
-             number_observers <= 5) %>% 
-      mutate(protocol_type = factor(protocol_type, 
-                                    levels = c("Stationary" , 
-                                               "Traveling")))
+# complete checklists only
+pred_data <- filter(ebird_habitat, as.logical(all_species_reported))
+# filter on effort covariates
+pred_data <- pred_data %>%
+    mutate(duration_minutes = as.numeric(as.character(duration_minutes))) %>%      
+    filter(protocol_type %in% c("Stationary", "Traveling"),
+           effort_distance_km <= 5,
+           duration_minutes <= 5 * 60,
+           number_observers <= 5) %>% 
+    mutate(protocol_type = factor(protocol_type, 
+                                  levels = c("Stationary" , 
+                                             "Traveling")))
 
-  # prepare for unmarked
-  occ <- filter_repeat_visits(pred_data, min_obs = 2, max_obs = 10,
-                              annual_closure = TRUE,
-                              date_var = "observation_date",
-                              site_vars = c("locality_id")) #, "observer_id"))
+# prepare for unmarked
+occ <- filter_repeat_visits(pred_data, min_obs = 2, max_obs = 10,
+                            annual_closure = TRUE,
+                            date_var = "observation_date",
+                            site_vars = c("locality_id")) #, "observer_id"))
 
-  if (sp_code == "woothr") {
-    plands <- paste0("pland_", c("04", "05", "12", "13"))
-  } else if (sp_code == "norbob") {
-    plands <- paste0("pland_", c("04", "08", "09", "13"))
-  } else if (sp_code == "whiibi") {
-    plands <- paste0("pland_", c("00", "08", "09", "11"))
-  } else {
-    stop("species code not valid.")
-  }
+if (sp_code == "woothr") {
+  plands <- paste0("pland_", c("04", "05", "12", "13"))
+} else if (sp_code == "norbob") {
+  plands <- paste0("pland_", c("04", "08", "09", "13"))
+} else if (sp_code == "whiibi") {
+  plands <- paste0("pland_", c("00", "08", "09", "11"))
+} else {
+  stop("species code not valid.")
+}
 
-  o_covs <- c("day_of_year",
-              "time_observations_started", 
-              "duration_minutes", 
-              "effort_distance_km", 
-              "number_observers", 
-              "protocol_traveling",
-              "checklist_calibration_index") %>% 
-    intersect(names(occ))
-  occ_wide <- occ %>%
-      mutate(latitude = round(latitude, 6)) %>%
-      mutate(longitude = round(longitude, 6)) %>%
-      format_unmarked_occu(site_id = "site", 
-                                   response = "species_observed",
-                                   site_covs = c("latitude", "longitude",
-                                                # habitat covariates
-                                                 plands),
-                                   obs_covs = o_covs)
+o_covs <- c("day_of_year",
+            "time_observations_started", 
+            "duration_minutes", 
+            "effort_distance_km", 
+            "number_observers", 
+            "protocol_traveling",
+            "checklist_calibration_index") %>% 
+  intersect(names(occ))
+occ_wide <- occ %>%
+    mutate(latitude = round(latitude, 6)) %>%
+    mutate(longitude = round(longitude, 6)) %>%
+    format_unmarked_occu(site_id = "site", 
+                                 response = "species_observed",
+                                 site_covs = c("latitude", "longitude",
+                                              # habitat covariates
+                                               plands),
+                                 obs_covs = o_covs)
+
+
+# spatial subsampling
+  # generate hexagonal grid
+  dggs <- dgconstruct(spacing = sample_spacing)
+  occ_wide_cell <- occ_wide %>% 
+    mutate(cell = dgGEO_to_SEQNUM(dggs, longitude, latitude)$seqnum)
+  # sample one record (set of repeated observations at a site) per grid cell
+  occ_wide <- occ_wide_cell %>% 
+    group_by(cell) %>% 
+    sample_n(size = 1) %>% 
+    ungroup() %>% 
+    select(-cell)
+
+# make unmarked object from the spatially subsampled data
+occ_um <- formatWide(occ_wide, type = "unmarkedFrameOccu")
   
-
-  # spatial subsampling
-    # generate hexagonal grid
-    dggs <- dgconstruct(spacing = sample_spacing)
-    occ_wide_cell <- occ_wide %>% 
-      mutate(cell = dgGEO_to_SEQNUM(dggs, longitude, latitude)$seqnum)
-    # sample one record (set of repeated observations at a site) per grid cell
-    occ_wide <- occ_wide_cell %>% 
-      group_by(cell) %>% 
-      sample_n(size = 1) %>% 
-      ungroup() %>% 
-      select(-cell)
-  
-  # make unmarked object from the spatially subsampled data
-  occ_um <- formatWide(occ_wide, type = "unmarkedFrameOccu")
-  
-
+det2 <- predict(mod_set$models[[1]], newdata = occ_um, type = "det")
 det3 <- predict(mod_set$models[[2]], newdata = occ_um, type = "det")
+det4 <- predict(mod_set$models[[3]], newdata = occ_um, type = "det")
+det5 <- predict(mod_set$models[[4]], newdata = occ_um, type = "det")
 det6 <- predict(mod_set$models[[5]], newdata = occ_um, type = "det")
 det7 <- predict(mod_set$models[[6]], newdata = occ_um, type = "det")
 
 
+plot_hist_pretty <- function(x, br = seq(0, 1, by=0.05), 
+  xlabel="", ylabel="", letter = 1, name = "", 
+  axis_x = FALSE, ...) {
+  h_counts <- hist(x, breaks = br, plot = FALSE)$counts
+  at_y <- pretty(c(0, max(h_counts)), 3)
+  hist(x, breaks = br, main = "", xaxt="n", yaxt="n", xlab=xlabel, ylab=ylabel, xpd = NA)
+  axis(side = 2, at = at_y, xpd = NA)
+  axis(side = 1, at = c(0, 0.5, 1), pos = 0, labels = rep("", 3))
+  if(axis_x) axis(side=1, at=c(0, 0.5, 1), pos = 0)
+  text(x = 0, y = max(at_y)*0.9, labels = LETTERS[letter], font = 2, pos = 4, cex = 0.98, xpd = NA)
+  text(x = 0.05, y = max(at_y)*0.9, labels = name, pos = 4, cex = 0.98, xpd = NA)
+}
+
+
 str_glue("{figure_folder}/occupancy-model_predictions_detectability_{sp_code}_{date}.png") %>% 
-  png(width = 14, height = 15, res = 600, pointsize = 9, units = "cm")
+  png(width = 13, height = 13, res = 600, pointsize = 9, units = "cm")
 
-br <- seq(0, 1, by=0.05)
-par(mfrow=c(2,1), mar = c(2, 5, 1, 1), oma = c(5, 1, 1, 1))
+par(mfrow=c(3,2), mar = c(2, 5, 2, 1), oma = c(3, 1, 4, 1))
 
-h_counts <- hist(det6$Predicted, breaks = br, plot = FALSE)$counts
-at_y <- pretty(c(0, max(h_counts)), 3)
-hist(det6$Predicted, breaks = br, xlab = "", ylab = "Frequency", main = "", xaxt="n", yaxt="n")
-axis(side = 1, at = c(0, 0.5, 1), pos = 0)
-axis(side = 2, at = at_y, xpd = NA)
-text(x = 0, y = max(at_y)*0.95, labels = "a", font = 2, pos = 4, cex = 0.98, xpd = NA)
-text(x = 0.03, y = max(at_y)*0.95, labels = "Model 6", pos = 4, cex = 0.98, xpd = NA)
+plot_hist_pretty(det2$Predicted, letter = 1, name = "Model 2")
+plot_hist_pretty(det3$Predicted, letter = 2, name = "Model 3")
 
-h_counts <- hist(det7$Predicted, breaks = br, plot = FALSE)$counts
-at_y <- pretty(c(0, max(h_counts)), 3)
-hist(det7$Predicted, breaks = br, xlab = "Estimated detectability", ylab = "Frequency", main = "", xaxt="n", xpd = NA, yaxt="n")
-axis(side=1, at=c(0, 0.5, 1), pos = 0)
-axis(side = 2, at = at_y, xpd = NA)
-text(x = 0, y = max(at_y)*0.95, labels = "b", font = 2, pos = 4, cex = 0.98, xpd = NA)
-text(x = 0.03, y = max(at_y)*0.95, labels = "Model 7", pos = 4, cex = 0.98, xpd = NA)
+plot_hist_pretty(det4$Predicted, letter = 3, name = "Model 4", ylabel = "Frequency")
+plot_hist_pretty(det5$Predicted, letter = 4, name = "Model 5")
+
+plot_hist_pretty(det6$Predicted, letter = 5, name = "Model 6", axis_x = TRUE, xlabel = "Estimated detectability")
+plot_hist_pretty(det7$Predicted, letter = 6, name = "Model 7", axis_x = TRUE, xlabel = "Estimated detectability")
 
 dev.off()
 
 
+
+# ####################################################################
+# VALIDATE ON THE HIGH DETECTABILITY SITES FROM 2017
+
+
 # --------------------------------------------------------------------
-# predictability on the 2017 data and aggregate within sites. 
+# predict on the 2017 data and aggregate within sites. 
 
 test_2017 <- ebird_habitat %>%
             filter(type == "test_2017") %>%
@@ -340,8 +356,7 @@ site_det <- sapply(test_2017_sp, FUN = aggregate_det)
 site_obs <- sapply(test_2017_sp, FUN = function(x){max(x$species_observed)})
 
 site_det_df <- data.frame(site = names(site_det), det = as.numeric(site_det), obs = as.numeric(site_obs))
-site_high_det <- site_det_df$site[site_det_df$det>0.95]
-
+site_high_det <- site_det_df$site[site_det_df$det>0.9]
 
 # separate out the sites with high detectability (e.g. det>0.9)
 test_2017_high_det <- test_2017 %>%
@@ -350,13 +365,14 @@ test_2017_high_det <- test_2017 %>%
           right_join(site_det_df, by="site") %>%
           filter(det>0.9)
 
-# predict occupancy at each site
+# predict occupancy at each high detectability site
 test_2017_high_det$occ <- predict(mod_set$models[[6]], newdata = test_2017_high_det, type = "state")$Predicted
 
 par(mfrow=c(2,1))
 br <- seq(0, 1, by = 0.05)
 hist(test_2017_high_det$occ[test_2017_high_det$obs==0], breaks = br)
 hist(test_2017_high_det$occ[test_2017_high_det$obs==1], breaks = br)
+
 
 # calculate ppms
 all_ppms <- map(mod_set$models, predict, newdata = test_2017_high_det, type = "state") %>%
@@ -377,43 +393,87 @@ plot_data <- all_ppms %>%
 
 
 # plot ppms
-  g_ppm <- ggplot(plot_data) +
-    aes(x = model, y = value) +
-    geom_point() +
-    facet_wrap(~ metric, nrow = 2, scales = "free_y") +
-    scale_y_continuous(breaks = c(0, 0.5, 1), limits=c(0, 1)) +
-    labs(x = NULL, y = NULL) +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1),
-          strip.text = element_text(size = 12, hjust = 0),
-          panel.background = element_rect(fill = "white"),
-          plot.background = element_rect(fill = "white"),
-          panel.border = element_rect(color = "black", fill = "transparent"),
-          axis.ticks.y = element_line(),
-          panel.grid = element_blank())
-  plotname <- str_glue("{figure_folder}/occu-model_2017_high_det_{sp_code}.png") %>%
-    ggsave(g_ppm, width = 20, height = 20, units = "cm", dpi = 300)
+g_ppm <- ggplot(plot_data) +
+  aes(x = model, y = value) +
+  geom_point() +
+  facet_wrap(~ metric, nrow = 2, scales = "free_y") +
+  scale_y_continuous(breaks = c(0, 0.5, 1), limits=c(0, 1)) +
+  labs(x = NULL, y = NULL) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        strip.text = element_text(size = 12, hjust = 0),
+        panel.background = element_rect(fill = "white"),
+        plot.background = element_rect(fill = "white"),
+        panel.border = element_rect(color = "black", fill = "transparent"),
+        axis.ticks.y = element_line(),
+        panel.grid = element_blank())
+plotname <- str_glue("{figure_folder}/occu-model_2017_high_det_{sp_code}.png") %>%
+  ggsave(g_ppm, width = 20, height = 20, units = "cm", dpi = 300)
 
 
+# ####################################################################
+# COMPARE OCCUPANCY ESTIMATES
 
 
-# number of observed positives
-
-obs <- occ_wide[,2:11]
-table(apply(obs, 1, max, na.rm = TRUE))  
- #   0    1 
- # 769 1318 
-
-
-occ_pred <- predict(mod_set$models[[6]], newdata = occ_um, type = "state")
+occ2 <- mod_set$r_pred[[1]]$occ_prob %>% as.matrix() %>% as.vector()
+occ3 <- mod_set$r_pred[[2]]$occ_prob %>% as.matrix() %>% as.vector()
+occ4 <- mod_set$r_pred[[3]]$occ_prob %>% as.matrix() %>% as.vector()
+occ5 <- mod_set$r_pred[[4]]$occ_prob %>% as.matrix() %>% as.vector()
+occ6 <- mod_set$r_pred[[5]]$occ_prob %>% as.matrix() %>% as.vector()
+occ7 <- mod_set$r_pred[[6]]$occ_prob %>% as.matrix() %>% as.vector()
 
 
-## find only the zero sites
-occ_zero <- occ_wide %>%
-            filter(pland_04==0, pland_05==0, pland_12==0, pland_13==0)
-# make unmarked object from the spatially subsampled data
-occ_zero_um <- formatWide(occ_zero, type = "unmarkedFrameOccu")
+str_glue("{figure_folder}/occupancy-model_predictions_occupancy_{sp_code}_{date}.png") %>% 
+  png(width = 13, height = 13, res = 600, pointsize = 9, units = "cm")
 
-mod <- occu(~day_of_year + time_observations_started + duration_minutes + 
-    effort_distance_km + number_observers + protocol_traveling ~ 1,
-    data = occ_zero_um)
+par(mfrow=c(3,2), mar = c(2, 5, 2, 1), oma = c(3, 1, 4, 1))
+
+plot_hist_pretty(occ2, letter = 1, name = "Model 2")
+plot_hist_pretty(occ3, letter = 2, name = "Model 3")
+
+plot_hist_pretty(occ4, letter = 3, name = "Model 4", ylabel = "Frequency")
+plot_hist_pretty(occ5, letter = 4, name = "Model 5")
+
+plot_hist_pretty(occ6, letter = 5, name = "Model 6", axis_x = TRUE, xlabel = "Estimated occupancy")
+plot_hist_pretty(occ7, letter = 6, name = "Model 7", axis_x = TRUE, xlabel = "Estimated occupancy")
+
+dev.off()
+
+
+# ####################################################################
+# SCATTER PLOT OF ALL OCCUPANCY ESTIMATES
+
+# prepare data
+pred_compare <- rasterToPoints(r_occ_prob) %>% 
+  as_tibble() %>% 
+  mutate(dummy = NA) %>%
+  select(-x, -y) %>% 
+  gather("run_name", "mod_type", -covs) %>% 
+  inner_join(mod_set %>% select(run_id, run_name) %>% rbind(c(1, "dummy")), by = "run_name") %>% 
+  select(run_id, mod_type, covs) %>%
+  arrange(run_id) %>% 
+  mutate(run = paste("Model", run_id)) %>%
+  filter(run!="Model 7") %>%
+  mutate(run = as_factor(run))
+
+
+# plot
+g_density <- ggplot(pred_compare) + 
+  aes(x = covs, y = mod_type) +
+  geom_hex(aes(fill = stat(count))) + 
+  stat_cor(method = "pearson") + 
+  scale_x_continuous(breaks = c(0, 0.5, 1)) + 
+  scale_y_continuous(breaks = c(0, 0.5, 1)) +
+  coord_equal() +
+  scale_fill_viridis_c(trans = "log10", labels = scales::comma) + 
+  facet_wrap(~ run, nrow = 2) + 
+  labs(x = "Model 7 predictions", y = "Model 2-6 predictions") + 
+  guides(fill = guide_colorbar(title = "# predictions", 
+                               title.position = "left",
+                               barwidth = 0.5, barheight = 12)) +
+  theme_few() +
+  theme(legend.position = "right",
+        legend.title = element_text(angle = 90, hjust = 0.5))
+str_glue("{figure_folder}/occu-model_scatter_density_{sp_code}.png")  %>% 
+  ggsave(g_density, width = 20, height = 12, units = "cm", dpi = 300)
+
